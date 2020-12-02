d61149ca58c108a949ae03f00f23cdc8dfce7f0d
Florian Dejonckheere <florian@nephroflow.com>
2020-12-02 08:26:35 +0100

    Add day two part one

---
+++ 2020/2/solution.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+passwords = File
+  .read("./input.txt")
+  .split("\n")
+  .map { |l| /(\d+)-(\d+) ([a-z]): ([a-z]+)/.match(l).to_a }
+
+# Part one
+puts passwords.count { |(_, m, n, c, p)| (m.to_i..n.to_i) === p.chars.tally[c] }
