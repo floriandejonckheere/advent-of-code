a52617c695a5d2c74eb6ae9cb4b2319fc8799ea0
Florian Dejonckheere <florian@nephroflow.com>
2020-12-03 09:49:02 +0100

    Add day two part two

--- 2020/3/solution.rb
+++ 2020/3/solution.rb
@@ -7,5 +7,18 @@ grid = File
   .split("\n")
   .map { |l| l.split("") }
 
+def trees(grid, x = 3, y = 1)
+  grid
+    .each_slice(y)
+    .filter_map
+    .with_index { |i, j| i.first[(x * j) % i.first.count] == "#" }
+    .count
+end
+
 # Part one
-puts grid.filter_map.with_index { |x, i| x[(3 * i) % x.count] == "#" }.count
+puts trees(grid, 3, 1)
+
+# Part two
+puts [[1, 1], [1, 3], [5, 1], [7, 1], [1, 2]]
+  .map { |(x, y)| trees(grid, x, y) }
+  .reduce(:*)
