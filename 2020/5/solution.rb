e5f3734c81f2c722d930b2ff8453e950505463ff
Florian Dejonckheere <florian@nephroflow.com>
2020-12-05 17:56:39 +0100

    Add day five part two

--- 2020/5/solution.rb
+++ 2020/5/solution.rb
@@ -6,13 +6,10 @@ seats = File
   .read("./input.txt")
   .split("\n")
 
-def decode(seat)
-  x = seat[0..6].tr("FB", "01").to_i(2)
-  y = seat[7..9].tr("LR", "01").to_i(2)
-
-  (x * 8) + y
-end
+ids = seats.map { |s| (s[0..6].tr("FB", "01").to_i(2) * 8) + s[7..9].tr("LR", "01").to_i(2) }
 
 # Part one
-puts seats.map { |s| decode s }.max
+puts ids.max
 
+# Part two
+puts (ids.min..ids.max).to_a - ids
