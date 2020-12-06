0f6091a9a86e5bb6e1c08650b319a719ec087d93
Florian Dejonckheere <florian@floriandejonckheere.be>
2020-12-06 14:06:06 +0100

    Add day six part two

--- 2020/6/solution.rb
+++ 2020/6/solution.rb
@@ -4,7 +4,11 @@
 
 forms = File
   .read("./input.txt")
+  .chomp("\n")
   .split("\n\n")
 
 # Part one
-puts forms.sum { |f| f.gsub(/[^a-z]/i, "").chars.uniq.count }
+puts forms.sum { |f| f.delete("\n").chars.uniq.count }
+
+# Part two
+puts forms.sum { |f| n = f.count("\n"); f.delete("\n").chars.tally.values.count { |v| v > n } }
