d0e118eb843cc2be3544c59f8ee10a467c4ca216
Florian Dejonckheere <florian@nephroflow.com>
2020-12-07 17:47:00 +0100

    Add day seven part two

--- 2020/7/solution.rb
+++ 2020/7/solution.rb
@@ -2,14 +2,12 @@
 
 # frozen_string_literal: true
 
-require "set"
-
 rules = File
   .read("./input.txt")
   .split("\n")
   .map { |r| /\A([a-z ]*) bags? contain (.*)\.\z/.match(r)[1..] }
   .to_h
-  .transform_values { |v| v.split(",").flat_map { |s| /([0-9]* ([a-z ]*)|no other) bags?/.match(s)[2..] }.compact }
+  .transform_values { |v| v.split(",").flat_map { |s| /(([0-9]*) ([a-z ]*)|no other) bags?/.match(s)[2..] }.compact }
 
 class Node
   attr_reader :label, :neighbours
@@ -19,13 +17,35 @@ class Node
     @neighbours = []
   end
 
+  def <<(node)
+    neighbours << node
+  end
+
   def flatten
     neighbours.flat_map(&:flatten).append(label)
   end
 end
 
-# Part one
-graph = Hash.new { |h, k| h[k] = Node.new(k) }
-rules.each { |key, values| values.each { |value| graph[value].neighbours << graph[key] } }
+class Graph
+  attr_reader :nodes
+
+  def initialize
+    @nodes = Hash.new { |h, k| h[k] = Node.new(k) }
+  end
+
+  def [](key)
+    nodes[key]
+  end
+end
+
+# Part one (inverted directed acyclic graph)
+graph = Graph.new
+rules.each { |key, values| values.each_slice(2) { |_number, value| graph[value] << graph[key] } }
 
 puts graph["shiny gold"].flatten.uniq.count - 1
+
+# Part two (directed acyclic graph)
+graph = Graph.new
+rules.each { |key, values| values.each_slice(2) { |number, value| number.to_i.times { graph[key] << graph[value] } } }
+
+puts graph["shiny gold"].flatten.count - 1
