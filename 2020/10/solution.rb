9f4d2f30d5577c0caf904e307a3a11dbaa7265f5
Florian Dejonckheere <florian@nephroflow.com>
2020-12-10 17:16:05 +0100

    Add day ten part two

--- 2020/10/solution.rb
+++ 2020/10/solution.rb
@@ -7,12 +7,27 @@ adapters = File
   .split("\n")
   .map(&:to_i)
 
-# Part one
 adapters = [0, *adapters.sort, adapters.max + 3]
 
+# Part one
 puts adapters
   .each_cons(2)
   .map { |a, b| b - a }
   .tally
   .values
   .reduce(:*)
+
+# Part two
+def traverse(adapters, results = {})
+  current = adapters.shift
+
+  return 1 if adapters.empty?
+
+  adapters
+    .filter_map
+    .with_index { |a, i| i unless a > current + 3 }
+    .map { |i| results[adapters[i]] ||= traverse(adapters[i..], results) }
+    .sum
+end
+
+puts traverse(adapters)
