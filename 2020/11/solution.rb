a2a48a68371d8aedcb9b12e89296d354391db18f
Florian Dejonckheere <florian@nephroflow.com>
2020-12-11 18:19:10 +0100

    Add day eleven part two

--- 2020/11/solution.rb
+++ 2020/11/solution.rb
@@ -2,8 +2,6 @@
 
 # frozen_string_literal: true
 
-require "byebug"
-
 seats = File
   .read("./input.txt")
   .split("\n")
@@ -33,6 +31,73 @@ loop do
   i += 1
 
   new = mutate(prev)
+
+  break if new.flatten == prev.flatten
+
+  prev = new
+end
+
+puts prev.flatten.count { |c| c == "#" }
+
+# Part two
+def zip(m, a, b)
+  a.zip(b).select { |(c, r)| !c.nil? && !r.nil? && m[r][c] != "." }.first
+end
+
+def adjacent2(m, r, c)
+  dim_r = m.count
+  dim_c = m.transpose.count
+
+  # CL (horizontal)
+  cl = (0..(c-1)).to_a.reverse
+  hl = [cl.select { |i| m[r][i] != "." }.first, r]
+
+  # CR
+  cr = ((c+1)..(dim_c-1)).to_a
+  hr = [cr.select { |i| m[r][i] != "." }.first, r]
+
+  # RU (vertical)
+  ru = (0..(r-1)).to_a.reverse
+  vu = [c, ru.select { |i| m[i][c] != "." }.first]
+
+  # RD
+  rd = ((r+1)..(dim_r-1)).to_a
+  vd = [c, rd.select { |i| m[i][c] != "." }.first]
+
+  [
+    hl, # Horizontal left
+    hr, # Horizontal right
+    vu, # Vertical up
+    vd, # Vertical down
+    zip(m, cl, ru), # Left up
+    zip(m, cl, rd), # Left down
+    zip(m, cr, ru), # Right up
+    zip(m, cr, rd), # Right down
+  ]
+    .compact
+    .count { |(c, r)| !c.nil? && !r.nil? && m[r][c] == "#" }
+end
+
+def mutate2(matrix)
+  adj = {}
+
+  matrix.map(&:dup).each_with_index do |row, r|
+    row.each_with_index do |elem, c|
+      (adj[r] ||= {})[c] ||= adjacent2(matrix, r, c)
+
+      row[c] = "#" if elem == "L" && adj[r][c] == 0
+      row[c] = "L" if elem == "#" && adj[r][c] >= 5
+    end
+  end
+end
+
+i = 0
+prev = seats
+
+loop do
+  i += 1
+
+  new = mutate2(prev)
 
   break if new.flatten == prev.flatten
 
