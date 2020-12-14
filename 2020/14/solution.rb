dbd1c9af3b39a346353e820f1b86c3758d0ab490
Florian Dejonckheere <florian@nephroflow.com>
2020-12-14 19:43:57 +0100

    Add day fourteen part two

--- 2020/14/solution.rb
+++ 2020/14/solution.rb
@@ -25,3 +25,39 @@ input.each do |i|
 end
 
 puts mem.values.sum
+
+# Part two
+def floatmask(mask, bits)
+  ones = mask.tr("X", "0").to_i(2)
+  bits |= ones
+
+  # Find indices of Xs
+  exes = mask.chars.reverse.filter_map.with_index { |x, i| i if x == "X" }
+
+  # Zero out Xs
+  bits &= ~exes.map { |b| 1 << b }.reduce(&:|)
+
+  (0..exes.length).flat_map do |length|
+    exes
+      .combination(length)                                 # Make all possible combination of indices of Xs
+      .filter_map { |i| i.map { |b| 1 << b }.reduce(&:|) } # Turn them into bitmaps
+      .prepend(0)                                          # Add zero bitmap
+      .map { |m| bits | m }                                # Apply bitmap
+  end
+end
+
+mask = nil
+mem = Hash.new(0)
+
+input.each do |i|
+  # Parse mask
+  next mask = i.split(" ").last if i.start_with? "mask"
+
+  # Parse address and value
+  adr, bits = *i.match(/\Amem\[([0-9]*)\] = ([0-9]*)\z/).to_a[1..]
+
+  # Generate floating addresses
+  floatmask(mask, adr.to_i).each { |a| mem[a] = bits.to_i }
+end
+
+puts mem.values.sum
